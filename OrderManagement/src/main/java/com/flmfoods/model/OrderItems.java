5eebc12ef28cad141f2fade3768c2bddde194e9c
Fayaz <kurubanethravathi2@gmail.com>
2025-10-04 12:49:10 +0530

    added POJO class for OrderManagement

---
+++ OrderManagement/src/main/java/com/flmfoods/model/OrderItems.java
@@ -0,0 +1,28 @@
+package com.flmfoods.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItems {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private long oredredItemId;
+	private long quantity;
+	
+	@ManyToOne
+	@JoinColumn(name="orderId",nullable = false)
+	private Order order;
+}
