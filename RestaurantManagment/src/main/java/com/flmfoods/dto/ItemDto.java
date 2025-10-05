fc5f748cb42580e9b465bc455a00296d74427b90
Bharath-Gadamsetty <venkatabharath.gadamsetty@gmail.com>
2025-10-05 11:40:08 +0530

    DTO's Updated...

---
+++ RestaurantManagment/src/main/java/com/flmfoods/dto/ItemDto.java
@@ -0,0 +1,18 @@
+package com.flmfoods.dto;
+
+public class ItemDto {
+
+private String itemName;
+	
+	private double itemPrice;
+	
+	private double itemRating;
+	
+	private boolean isItemAvailable;
+	
+	private String itemType;
+	
+	private String itemCategory;
+	
+	private RestaurantDto restaurantDto;
+}
---
+++ RestaurantManagment/src/main/java/com/flmfoods/dto/RestaurantDto.java
@@ -0,0 +1,15 @@
+package com.flmfoods.dto;
+
+import java.util.List;
+
+public class RestaurantDto {
+
+	private String restaurantName;
+	
+	private String phoneNum;
+	
+	private double rating;
+	
+	private List<ItemDto> items;
+
+}
